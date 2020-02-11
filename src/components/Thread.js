632656f1d36f9936b8e7505262dafbe4cd31658d
jriley15 <jordanr3@live.com>
2020-02-10 21:28:44 -0700

    sdfsdf

---
+++ src/components/Thread.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useRef, useState } from "react";
+
+export default function Thread({ namespaceId, threadId }) {
+  const [height, setHeight] = useState(500);
+  const iframe = useRef();
+
+  useEffect(() => {
+    const handler = event => {
+      if (event.data.height) setHeight(event.data.height + 16);
+    };
+
+    window.addEventListener("message", handler);
+
+    return () => {
+      window.removeEventListener("message", handler);
+    };
+  }, []);
+
+  useEffect(() => {
+    iframe.current.contentWindow.postMessage(
+      { outerHeight: window.top.outerHeight },
+      "*"
+    );
+    iframe.current.contentWindow.postMessage(
+      { outerWidth: window.top.outerWidth },
+      "*"
+    );
+    iframe.current.contentWindow.postMessage(
+      { screenY: window.top.screenY },
+      "*"
+    );
+    iframe.current.contentWindow.postMessage(
+      { screenX: window.top.screenX },
+      "*"
+    );
+  }, [iframe]);
+
+  return (
+    <iframe
+      title="JThreads"
+      src={`https://embed.jrdn.tech/?namespaceId=${namespaceId}&threadId=${threadId}`}
+      width="100%"
+      allowTransparency="true"
+      frameBorder="0"
+      ref={iframe}
+      scrolling="no"
+      style={{
+        overflow: "hidden",
+        height: height
+      }}
+    />
+  );
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,23 @@
-// webpack.config.js
 var path = require("path");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: "./index.js",
+  mode: "production",
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve("build"),
     filename: "index.js",
     libraryTarget: "commonjs2"
   },
   module: {
     rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
       {
-        test: /\.js$/,
-        include: path.resolve(__dirname, "src"),
-        exclude: /(node_modules|bower_components|build)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["env"]
-          }
-        }
-      },
-      {
-        test: /\.*css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: ["css-loader", "sass-loader"]
-        })
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
       }
     ]
   },
   externals: {
-    react: "commonjs react"
+    react: "react"
   }
 };
